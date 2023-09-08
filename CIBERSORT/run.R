a9611ba345fda1fe06c1858e8476bd3206e31738
zxl2014swjx <54253876+zxl2014swjx@users.noreply.github.com>
2023-09-08 10:22:15 +0800

    Update run.R

--- CIBERSORT/run.R
+++ CIBERSORT/run.R
@@ -5,8 +5,6 @@
 #    install.packages("BiocManager")
 #BiocManager::install("preprocessCore", version = "3.8")
 
-
-#setwd("C:\\Users\\lexb4\\Desktop\\TCGAimmune\\06.CIBERSORT")
 source("CIBERSORT.R")
 results=CIBERSORT("ref.txt", "need.txt", perm=100, QN=TRUE)
 
