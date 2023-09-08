44c253fce728454cb178eb2a809c2c015279aea5
zxl2014swjx <54253876+zxl2014swjx@users.noreply.github.com>
2023-09-08 10:23:12 +0800

    Update run.R

--- CIBERSORT/run.R
+++ CIBERSORT/run.R
@@ -5,6 +5,10 @@
 #    install.packages("BiocManager")
 #BiocManager::install("preprocessCore", version = "3.8")
 
+#"need.txt" input_matrix
+#"ref.txt"  input_markers
+
+
 source("CIBERSORT.R")
 results=CIBERSORT("ref.txt", "need.txt", perm=100, QN=TRUE)
 
